8b425ded28873dba291446e59485734ee240f898
adrianrivers <adrianlrivers@gmail.com>
2022-04-11 00:43:55 +1200

    :recycle: Add basic styling to links

--- packages/router/src/views/AboutView.vue
+++ packages/router/src/views/AboutView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="about">
     <h1>About</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec maximus risus, sed
+      sagittis dolor. Vivamus non aliquam leo. Suspendisse cursus porttitor felis, mattis sodales
+      diam vestibulum eu. Nulla euismod magna eu congue tempor. Phasellus euismod gravida nisl a
+      tempor. Proin fringilla auctor ante in aliquam. In congue odio mi. Etiam tellus sapien, cursus
+      id libero in, interdum malesuada enim.
+    </p>
+    <p>
+      Phasellus luctus mauris ut nisi posuere lacinia. Aliquam erat volutpat. Cras sit amet tortor
+      velit. Etiam ullamcorper gravida feugiat. Phasellus ut varius sapien. Aenean euismod ac nunc
+      eu pharetra. Aenean ut nisi leo. In hac habitasse platea dictumst.
+    </p>
   </div>
 </template>
 
--- packages/router/src/views/HomeView.vue
+++ packages/router/src/views/HomeView.vue
@@ -1,8 +1,25 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
     <h1>Home</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel egestas libero. Pellentesque
+      lacinia lectus nec ultricies sodales. Sed mollis tempor felis, nec gravida mi euismod ac.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      Nullam ac nunc erat. Donec tristique lacus id augue mattis pretium. Fusce vestibulum nisi
+      lacus, nec volutpat odio tempus ut.
+    </p>
+
+    <p>
+      Vestibulum sollicitudin auctor purus vel vulputate. Donec vitae nulla congue, sodales nisi
+      vel, gravida magna. Sed posuere ut tortor id condimentum. Aliquam ac quam vel ante suscipit
+      luctus non id mauris. Phasellus quis neque nec dui maximus ornare. Ut pharetra consequat
+      maximus. Phasellus augue turpis, commodo gravida commodo eget, hendrerit sit amet velit.
+      Suspendisse id condimentum tellus. Maecenas elementum odio vel molestie pretium. Praesent
+      egestas ligula vitae nisl congue venenatis. Integer eget turpis sed tortor rhoncus finibus ac
+      sit amet velit. In sapien leo, faucibus ut purus nec, ullamcorper rhoncus nulla.
+    </p>
   </main>
 </template>
